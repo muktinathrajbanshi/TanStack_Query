b1f70047b07f77c4add92f3b0b0f98398b03d82c
unknown <muktinathrajbanshi786@gmail.com>
2025-11-22 14:42:00 +0530

    creating router for defining path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+// Creating a Router 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/trad",
+        element: <fetchOld />,
+      },
+      {
+        path: "/rq",
+        element: <fetchRQ />,
+      },
+    ],
+  },
+]);
+
+
 const App = () => {
-  return <h1>Hello, React Query!!!</h1>
+  return <h1>Hello, TanStack Query!!!</h1>
 }
 
 export default App;
