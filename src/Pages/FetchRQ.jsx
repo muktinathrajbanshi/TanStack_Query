287227eef311fe067335095cf476ac9192d0164b
unknown <muktinathrajbanshi786@gmail.com>
2025-11-23 14:36:01 +0530

    now it's time for react query implementation which is new

--- src/Pages/FetchRQ.jsx
+++ src/Pages/FetchRQ.jsx
@@ -1,3 +1,42 @@
+import { useEffect } from "react";
+import { fetchPosts } from "../API/api";
+
 export const FetchRQ = () => {
-    return <h1>FetchRQ Page!</h1>
+
+
+    const getPostData = async () => {
+        try {
+            const res = await fetchPosts();
+            // console.log(res);
+            res.status === 200 ? setPosts(res.data) : [];
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    };
+
+    useEffect(() => {
+        getPostData();
+    }, [])
+
+    useQuery({
+        queryKey:["posts"], //useState
+        queryFn:getPostData() // useEffect
+    });
+
+    return (
+        <div>
+            <ul className="section-accordion">
+                {posts?.map((curElem) => {
+                    const {id, title, body} = curElem;
+                    return (
+                        <li key={id}>
+                            <p>{title}</p>
+                            <p>{body}</p>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 };
