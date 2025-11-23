34fb590cdf89f54f288f8b6af5a2652dde468cad
unknown <muktinathrajbanshi786@gmail.com>
2025-11-23 14:23:44 +0530

    wow data is received and read also by get method

--- src/Pages/FetchOld.jsx
+++ src/Pages/FetchOld.jsx
@@ -21,6 +21,18 @@ export const FetchOld = () => {
     }, [])
 
     return (
-        <h1>hello FetchOld</h1>
+        <div>
+            <ul className="section-accordion">
+                {posts.map((curElem) => {
+                    const {id, title, body} = curElem;
+                    return (
+                        <li key={id}>
+                            <p>{title}</p>
+                            <p>{body}</p>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
     );
 };
