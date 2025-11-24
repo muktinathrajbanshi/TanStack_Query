606077c8676f433907ea1d364a3763776e1c5cd8
unknown <muktinathrajbanshi786@gmail.com>
2025-11-24 13:22:01 +0200

    wow what a icon of the react query devtool

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router}></RouterProvider>;
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
