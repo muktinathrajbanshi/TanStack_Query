1196ba43dbf8ac6febd6c8c78d9eaa902ab14746
unknown <muktinathrajbanshi786@gmail.com>
2025-11-24 14:19:17 +0200

    adding css for footer section also

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,7 +1,9 @@
 export const Footer = () => {
     return (
         <footer>
+        <div className="footer-page">
             <p>copyright @MuktinathTech</p>
+        </div>
         </footer>
     );
 };
