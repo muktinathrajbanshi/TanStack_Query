6906c36295bfe82eb99ba568ef0a356a6e9f2acc
unknown <muktinathrajbanshi786@gmail.com>
2025-11-25 16:46:50 +0530

    wow what a logic

--- src/components/UI/FetchIndv.jsx
+++ src/components/UI/FetchIndv.jsx
@@ -1,3 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { fetchInvPost } from "../../API/api";
+
 export const FetchIndv = () => {
-    return <h1>Hello Individual</h1>;
+    const { id } = useParams();
+    
+    const { data, isLoading, isError, error } = useQuery({
+        queryKey: ["post"], //useState
+        queryFn: () => fetchInvPost(id), // useEffect
+    });
+
+    console.log(data);
+    
+
+    return <h1>Hello {id}</h1>;
 };
