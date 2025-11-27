b6d125ac6c52c7f28b6ea247c1594d75f057d353
unknown <muktinathrajbanshi786@gmail.com>
2025-11-27 10:05:22 +0400

    Oh my god it's now happening

--- src/Pages/InfiniteScroll.jsx
+++ src/Pages/InfiniteScroll.jsx
@@ -15,5 +15,28 @@ export const InfiniteScroll = () => {
     console.log(data);
     
 
-    return <h1>Infinite Scrool!</h1>;
+    return (
+        <div>
+            <h1>Infinite Scroll with React Query v5</h1>
+
+            {data?.pages?.map((page, index) => (
+                <ul key={index}>
+                    {page.map((user) => (
+                        <li
+                        key={user.id}
+                        style={{ padding: "10px", border: "1px solid #ccc" }}
+                        >
+                        <p>{user.login}</p>
+                        <img
+                        src={user.avatar_url}
+                        alt={user.login}
+                        width={50}
+                        height={50}
+                         />
+                        </li>
+                    ))}
+                </ul>
+            ))}
+        </div>
+    )
 }
