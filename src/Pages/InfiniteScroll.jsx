598c18f40bdd5a9d40badb10bee6e9d4dd6d5ccf
unknown <muktinathrajbanshi786@gmail.com>
2025-11-27 09:55:58 +0400

    Oh my goodness what a logic for instagram like structure

--- src/Pages/InfiniteScroll.jsx
+++ src/Pages/InfiniteScroll.jsx
@@ -1,3 +1,19 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { fetchUsers } from "../API/api";
+
 export const InfiniteScroll = () => {
+
+   const { data } = useInfiniteQuery({
+        queryKey:["users"],
+        queryFn:fetchUsers,
+        getNextPageParam: (lastPage, allPages) => {
+            console.log("lastPage", lastPage, allPages);
+            return lastPage.length === 10 ? allPages.length + 1 : undefined;
+        },
+    });
+
+    console.log(data);
+    
+
     return <h1>Infinite Scrool!</h1>;
 }
